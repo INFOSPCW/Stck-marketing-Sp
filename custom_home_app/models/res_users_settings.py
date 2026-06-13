7fd1921ef75710562d6cfde8cf015d58a7f32707
Robert <info@solutionprovidercw.com>
2026-06-13 14:39:03 +0000

    Cleanup: remove unused and incompatible modules

--- custom_home_app/models/res_users_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResUsersSettings(models.Model):
-    _inherit = "res.users.settings"
-
-    home_menu_layout = fields.Json(string="Home Menu Layout", readonly=True)
--- theme_diwy/doc/RELEASE_NOTES.md
+++
@@ -1,8 +0,0 @@
-## Module <theme_diwy>
-#### 21.02.2026
-#### Version 19.0.1.0.0
-##### ADD
-
-- Initial commit for Theme Diwy 
-
-
